b364a05612d943c819ef7d18b1db10dccedc12d5
pancake <pancake@nopcode.org>
2017-11-27 11:48:15 +0100

    Update Github public key certificate

--- src/org/radare/radare2installer/PubKeyManager.java
+++ src/org/radare/radare2installer/PubKeyManager.java
@@ -20,6 +20,7 @@ public final class PubKeyManager implements X509TrustManager {
 	// base64 -D > rawcert.pub ; vim rawcert.pub
 	// rax2 -S < rawcert.pub > rawcert.txt
 	private static String PUB_KEY = ""
+/*
 		+ "30820122300d06092a864886f70d01010105000382010f003082010a"
 		+ "0282010100fbd5940a0ae050dc0ffc90b771479f2c05de0e9abc2a8f"
 		+ "d4f29f0846f9f2d118b423a52ad2df913ff9c5d0b240bdd6bc40762e"
@@ -31,6 +32,8 @@ public final class PubKeyManager implements X509TrustManager {
 		+ "536f7040da89294f0c097ea3ecc557ad03aa91ed435cf9f55be8a1f0"
 		+ "be6d1bce2dab437c70dc3fecc911f074c929a150d03c2938dc7f56b9"
 		+ "f81f04a45e9fcedd170203010001";
+*/
++ "30820122300d06092a864886f70d01010105000382010f003082010a0282010100c6d3f18a3bcfa445f2cb7067d7459fa1698a4d6ef9dd4bf63eeb033666a5c7fee6a85aa2e41a8ae315901d0812a7285e760b562175822461ed80555c93e0c101b1e21ec13aedec295756b69761a9a8d0854d4efb52ca0d543ff13f2c7793e70f5fdcbcaea8cc899077c6cd7328360191ca0156b03e88edf6dd89099822c45c23b63bb6f5b702c55a437031dedeee7b5ebb6b8232fc4da79420db63089f7dedd9e80c3df20353f4dc2837f26adcb9face85de0ce1ede2209ea3503744ffe5fa5a624a9dc7c8f6d500ec23217f09f4a9039a8a2ee865baef31ad46e7734322817ed54e14bd3db7f131243571041f6c6771a103494cd1f15eff994d70312828eee70203010001";
 
 	public void checkServerTrusted(X509Certificate[] chain, String authType)
 			throws CertificateException {
